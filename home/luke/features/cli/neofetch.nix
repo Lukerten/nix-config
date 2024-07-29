e9d0932caf53588d227c02ba17f52d9d62f29157
Lucas Brendgen <lc.brendgen@gmail.com>
2024-07-29 11:15:16 +0200

    fix: adding imagemagick

--- home/luke/features/cli/neofetch.nix
+++ home/luke/features/cli/neofetch.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ neofetch ];
+  home.packages = with pkgs; [ neofetch imagemagick ];
   xdg.configFile."neofetch/config.conf".source = ./neofetch.conf;
 }
