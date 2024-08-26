797dca86b01bcac078c094203dafc92355424ce2
Lucas Brendgen <lc.brendgen@gmail.com>
2024-08-26 22:51:56 +0200

    fix: regreet following gtk theming

--- hosts/common/optional/regreet.nix
+++ hosts/common/optional/regreet.nix
@@ -6,50 +6,40 @@
 }: let
   homeCfgs = config.home-manager.users;
   homeSharePaths = lib.mapAttrsToList (_: v: "${v.home.path}/share") homeCfgs;
-  vars = ''
-    XDG_DATA_DIRS="$XDG_DATA_DIRS:${
-      lib.concatStringsSep ":" homeSharePaths
-    }" GTK_USE_PORTAL=0'';
+  vars = ''XDG_DATA_DIRS="$XDG_DATA_DIRS:${lib.concatStringsSep ":" homeSharePaths}" GTK_USE_PORTAL=0'';
 
   lukeCfg = homeCfgs.luke;
-  gtkTheme = lukeCfg.gtk.theme;
-  iconTheme = lukeCfg.gtk.iconTheme;
-  wallpaper = lukeCfg.wallpaper;
 
-  sway-kiosk = command: "${lib.getExe pkgs.sway} --unsupported-gpu --config ${
-    pkgs.writeText "kiosk.config" ''
-      output * bg #000000 solid_color
-      xwayland disable
-      input "type:touchpad" {
-        tap enabled
-      }
-      exec '${vars} ${command}; ${pkgs.sway}/bin/swaymsg exit'
+  sway-kiosk = command: "${lib.getExe pkgs.sway} --unsupported-gpu --config ${pkgs.writeText "kiosk.config" ''
+    output * bg #000000 solid_color
+    xwayland disable
+    input "type:touchpad" {
+      tap enabled
+    }
+    exec '${vars} ${command}; ${pkgs.sway}/bin/swaymsg exit'
     ''
   }";
 in {
-  users.extraUsers.greeter = {
-    packages = [gtkTheme.package iconTheme.package];
+ users.extraUsers.greeter = {
     # For caching and such
     home = "/tmp/greeter-home";
     createHome = true;
   };
-
   programs.regreet = {
     enable = true;
-    settings = {
-      GTK = {
-        icon_theme_name = lib.mkForce "Papirus";
-        theme_name = lib.mkForce gtkTheme.name;
-      };
-      background = {
-        path = wallpaper;
-        fit = "Cover";
-      };
+    iconTheme = lukeCfg.gtk.iconTheme;
+    theme = lukeCfg.gtk.theme;
+    cursorTheme = {
+      inherit (lukeCfg.gtk.cursorTheme) name package;
+    };
+    font.name = lukeCfg.fontProfiles.regular.family;
+    settings.background = {
+      path = lukeCfg.wallpaper;
+      fit = "Cover";
     };
   };
   services.greetd = {
     enable = true;
-    settings.default_session.command =
-      sway-kiosk (lib.getExe config.programs.regreet.package);
+    settings.default_session.command = sway-kiosk (lib.getExe config.programs.regreet.package);
   };
 }
