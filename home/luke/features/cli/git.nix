439dcfff9b177de056a90bfc9f73ebeda985984d
Lucas Brendgen <lc.brendgen@gmail.com>
2024-08-26 22:53:12 +0200

    feat: git sweep

--- home/luke/features/cli/git.nix
+++ home/luke/features/cli/git.nix
@@ -19,6 +19,7 @@ in {
     package = pkgs.gitAndTools.gitFull;
     aliases = {
       p = "pull --ff-only";
+      sweep = "fetch --prune && branch -vv | grep ': gone]' | awk '{print $1}' | xargs -r git branch -d";
       ff = "merge --ff-only";
       graph = "log --decorate --oneline --graph";
       pushall = "!git remote | xargs -L1 git push --all";
