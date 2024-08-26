00f86c82f01c8ba522683940dcdc60a30ef34679
Lucas Brendgen <lc.brendgen@gmail.com>
2024-08-26 22:53:44 +0200

    feat: using hyprcursor instead of xdg-cursors

--- home/luke/features/desktop/hypr/default.nix
+++ home/luke/features/desktop/hypr/default.nix
@@ -153,7 +153,10 @@ in {
         ];
       };
 
-      exec = ["${pkgs.swaybg}/bin/swaybg -i ${config.wallpaper} --mode fill"];
+      exec = [
+        "${pkgs.swaybg}/bin/swaybg -i ${config.wallpaper} --mode fill"
+        "hyprctl setcursor ${config.gtk.cursorTheme.name} ${toString config.gtk.cursorTheme.size}"
+      ];
 
       monitor = let
         waybarSpace = let
