0f2bcaccbf76c2f43585b2aeb79d26544342dc73
Lucas Brendgen <lc.brendgen@gmail.com>
2024-09-05 08:26:51 +0200

    feat(home/cups): adding extra drivers

--- hosts/common/optional/cups.nix
+++ hosts/common/optional/cups.nix
@@ -1,1 +1,41 @@
-{services.printing.enable = true;}
+{pkgs, ...}: let
+  printing-drivers = [
+    pkgs.gutenprint
+    pkgs.gutenprintBin
+    pkgs.hplip
+    pkgs.hplipWithPlugin
+    pkgs.postscript-lexmark
+    pkgs.samsung-unified-linux-driver
+    pkgs.splix
+    pkgs.brlaser
+    pkgs.brgenml1lpr
+    pkgs.brgenml1cupswrapper
+    pkgs.cnijfilter2
+
+  ];
+in {
+  services.printing = {
+  enable = true;
+  allowFrom = [ "all" ];
+  browsing = true;
+  defaultShared = true;
+  browsedConf = ''
+    BrowseDNSSDSubTypes _cups,_print
+    BrowseLocalProtocols all
+    BrowseRemoteProtocols all
+    CreateIPPPrinterQueues All
+    BrowseProtocols all
+    '';
+  drivers = printing-drivers;
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+}
