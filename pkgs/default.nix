6301b6448e75664468a0ce430b50f437853ad90d
Lucas Brendgen <lc.brendgen@gmail.com>
2024-09-06 14:15:28 +0200

    cleanup: removing redundant commitmsg tool

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,9 @@
 {pkgs ? import <nixpkgs> {}, ...}: rec {
-  # Packages with an actual source
   rgbdaemon = pkgs.callPackage ./rgbdaemon {};
   shellcolord = pkgs.callPackage ./shellcolord {};
   trekscii = pkgs.callPackage ./trekscii {};
   compiz = pkgs.callPackage ./compiz {};
-
-  # Luke's Custom packages
   cm4all-vpn = pkgs.callPackage ./cm4all-vpn {};
-  commitmsg = pkgs.callPackage ./commitmsg {};
 
   # stolen from misterio77
   nix-inspect = pkgs.callPackage ./nix-inspect {};
