1fa2a28dc544d5d20943d97d8df65a37b9ddb52d
Lucas Brendgen <lc.brendgen@gmail.com>
2024-09-13 02:36:53 +0200

    feat: imv desktop entries

--- home/luke/features/desktop/common-wayland/imv.nix
+++ home/luke/features/desktop/common-wayland/imv.nix
@@ -1,1 +1,41 @@
-{programs.imv.enable = true;}
+{config,...}:{
+  programs.imv = {
+    enable = true;
+    settings = {
+      options.background = config.colorscheme.colors.surface;
+    };
+  };
+  xdg.desktopEntries = {
+    imv = {
+      exec = "imv %F";
+      name = "imv";
+      mimeType = [
+        "image/jpeg"
+        "image/png"
+        "image/gif"
+        "image/bmp"
+        "image/svg+xml"
+        "image/tiff"
+        "image/x-xcf"
+        "image/x-xbitmap"
+        "image/x-xpixmap"
+      ];
+      terminal = false;
+      type = "Application";
+      categories = ["Utility" "FileManager"];
+      settings.NoDisplay = "true";
+    };
+  };
+
+  xdg.mimeApps.defaultApplications = {
+    "image/jpeg" = [ "imv.desktop" ];
+    "image/png" = [ "imv.desktop" ];
+    "image/gif" = [ "imv.desktop" ];
+    "image/bmp" = [ "imv.desktop" ];
+    "image/svg+xml" = [ "imv.desktop" ];
+    "image/tiff" = [ "imv.desktop" ];
+    "image/x-xcf" = [ "imv.desktop" ];
+    "image/x-xbitmap" = [ "imv.desktop" ];
+    "image/x-xpixmap" = [ "imv.desktop" ];
+  };
+}
