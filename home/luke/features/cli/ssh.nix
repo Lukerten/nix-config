d58665f11d12da23112773cde43025d054e0763b
Lucas Brendgen <lucas.brendgen@gmail.com>
2024-09-18 00:22:54 +0200

    feat: adding cm hosts

--- home/luke/features/cli/ssh.nix
+++ home/luke/features/cli/ssh.nix
@@ -1,6 +1,7 @@
 {
   outputs,
   lib,
+  config,
   ...
 }: let
   hostnames = builtins.attrNames outputs.nixosConfigurations;
@@ -10,10 +11,34 @@ in {
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    matchBlocks = {
+    matchBlocks = let
+      home = config.home.homeDirectory;
+    in {
       net = {
         host = builtins.concatStringsSep " " hostnames;
         forwardAgent = true;
+      };
+      "gox-lucasb" = {
+        hostname = "gox-lucasb.os.intern.cm-ag";
+        user = "debian";
+        identityFile = "${home}/.ssh/id_Lucas.Brendgen.rsa";
+        forwardAgent = true;
+      };
+      "smb-server" = {
+        hostname = "*.smb-server";
+        forwardAgent = false;
+        identityFile = "${home}/.ssh/id_Lucas.Brendgen.rsa";
+      };
+      "t8o.de" = {
+        hostname = "*.t8o.de";
+        forwardAgent = false;
+        identityFile = "${home}/.ssh/id_Lucas.Brendgen.rsa";
+      };
+      "hye-fhe" = {
+        hostname = "hye.ai.fh-erfurt.de";
+        user = "lucasb";
+        identityFile = "${home}/.ssh/id_fsr.Lucas.Brendgen.rsa";
+        forwardAgent = false;
       };
     };
   };
