fbd8799289523d3c147b3df9016fb7518e887b1d
Lucas Brendgen <lc.brendgen@gmail.com>
2024-10-22 22:25:54 +0200

    feat(home/cli): implementing tide fish prompt

--- home/luke/features/cli/fish/default.nix
+++ home/luke/features/cli/fish/default.nix
@@ -3,11 +3,15 @@
   config,
   ...
 }: {
-  programs.fish = {
+  imports = [./tide.nix];
+  programs.fish = let
+    hasEza = lib.mkIf config.programs.eza.enable;
+    hasNeovim = config.programs.neovim.enable;
+    hasRipgrep = config.programs.ripgrep.enable;
+    inherit (lib) mkIf;
+  in {
     enable = true;
-    shellAbbrs = let
-      hasEza = lib.mkIf config.programs.eza.enable;
-    in
+    shellAbbrs =
       config.home.shellAliases
       // {
         ls = hasEza "exa";
@@ -17,8 +21,58 @@
       };
     functions = {
       fish_greeting = "";
+      nvimrg = mkIf (hasNeovim && hasRipgrep) "nvim -q (rg --vimgrep $argv | psub)";
       up-or-search = lib.readFile ./up-or-search.fish;
+      nix-inspect =
+        /*
+        fish
+        */
+        ''
+          set -s PATH | grep "PATH\[.*/nix/store" | cut -d '|' -f2 |  grep -v -e "-man" -e "-terminfo" | perl -pe 's:^/nix/store/\w{32}-([^/]*)/bin$:\1:' | sort | uniq
+        '';
     };
-    interactiveShellInit = lib.readFile ./interactive-init.fish;
+    interactiveShellInit =
+      /*
+      fish
+      */
+      ''
+        bind \ee edit_command_buffer
+
+        # Use vim bindings and cursors
+        fish_vi_key_bindings
+        set fish_cursor_default     block      blink
+        set fish_cursor_insert      line       blink
+        set fish_cursor_replace_one underscore blink
+        set fish_cursor_visual      block
+
+        # Use terminal colors
+        set -x fish_color_autosuggestion      brblack
+        set -x fish_color_cancel              -r
+        set -x fish_color_command             brgreen
+        set -x fish_color_comment             brmagenta
+        set -x fish_color_cwd                 green
+        set -x fish_color_cwd_root            red
+        set -x fish_color_end                 brmagenta
+        set -x fish_color_error               brred
+        set -x fish_color_escape              brcyan
+        set -x fish_color_history_current     --bold
+        set -x fish_color_host                normal
+        set -x fish_color_host_remote         yellow
+        set -x fish_color_match               --background=brblue
+        set -x fish_color_normal              normal
+        set -x fish_color_operator            cyan
+        set -x fish_color_param               brblue
+        set -x fish_color_quote               yellow
+        set -x fish_color_redirection         bryellow
+        set -x fish_color_search_match        'bryellow' '--background=brblack'
+        set -x fish_color_selection           'white' '--bold' '--background=brblack'
+        set -x fish_color_status              red
+        set -x fish_color_user                brgreen
+        set -x fish_color_valid_path          --underline
+        set -x fish_pager_color_completion    normal
+        set -x fish_pager_color_description   yellow
+        set -x fish_pager_color_prefix        'white' '--bold' '--underline'
+        set -x fish_pager_color_progress      'brwhite' '--background=cyan'
+      '';
   };
 }
