f446df9ae4d1301010aa7c7f1ad1405803c3b898
Lucas Brendgen <lc.brendgen@gmail.com>
2024-10-22 11:16:21 +0200

    fix(home/nvim): hoved nvim xdg entry to module
    added xdg-entry option to module
    also refactored the code to be more readable

--- home/luke/features/nvim/default.nix
+++ home/luke/features/nvim/default.nix
@@ -13,55 +13,36 @@
     pvim.enable = true;
   };
 
-  xdg = let
-    TermLaunch = ''${lib.getExe pkgs.handlr-regex} launch "x-scheme-handler/terminal" -- -e'';
-    mimeAppsList = {
-      "text/english" = "nvim.desktop";
-      "text/plain" = "nvim.desktop";
-      "text/x-makefile" = "nvim.desktop";
-      "text/x-java" = "nvim.desktop";
-      "text/x-pascal" = "nvim.desktop";
-      "text/x-lua" = "nvim.desktop";
-      "text/x-c" = "nvim.desktop";
-      "text/x-c++" = "nvim.desktop";
-      "text/x-csrc" = "nvim.desktop";
-      "text/x-csharp" = "nvim.desktop";
-      "text/x-c++hdr" = "nvim.desktop";
-      "text/x-c++src" = "nvim.desktop";
-      "text/x-tex" = "nvim.desktop";
-      "text/x-moc" = "nvim.desktop";
-      "text/x-chdr" = "nvim.desktop";
-      "text/x-tcl" = "nvim.desktop";
-      "application/javascript" = "nvim.desktop";
-      "application/json" = "nvim.desktop";
-      "application/json+ld" = "nvim.desktop";
-      "application/yaml" = "nvim.desktop";
-      "application/toml" = "nvim.desktop";
-      "application/xml" = "nvim.desktop";
-      "application/x-shellscript" = "nvim.desktop";
-      "application/x-cmake" = "nvim.desktop";
-      "application/x-yaml" = "nvim.desktop";
-      "application/x-tex" = "nvim.desktop";
-      "application/x-ruby" = "nvim.desktop";
-      "application/x-python" = "nvim.desktop";
-      "application/x-perl" = "nvim.desktop";
-      "application/x-lua" = "nvim.desktop";
-    };
-  in {
-    desktopEntries = {
-      nvim = {
-        name = "Neovim";
-        genericName = "Text Editor";
-        comment = "Edit text files";
-        exec = "${TermLaunch} nvim %F";
-        icon = "nvim";
-        mimeType = builtins.attrNames mimeAppsList;
-        terminal = false;
-        type = "Application";
-        categories = ["Utility" "TextEditor"];
-      };
-    };
-
-    mimeApps.defaultApplications = mimeAppsList;
+  xdg.mimeApps.defaultApplications = {
+    "text/english" = "nvim.desktop";
+    "text/plain" = "nvim.desktop";
+    "text/x-makefile" = "nvim.desktop";
+    "text/x-java" = "nvim.desktop";
+    "text/x-pascal" = "nvim.desktop";
+    "text/x-lua" = "nvim.desktop";
+    "text/x-c" = "nvim.desktop";
+    "text/x-c++" = "nvim.desktop";
+    "text/x-csrc" = "nvim.desktop";
+    "text/x-csharp" = "nvim.desktop";
+    "text/x-c++hdr" = "nvim.desktop";
+    "text/x-c++src" = "nvim.desktop";
+    "text/x-tex" = "nvim.desktop";
+    "text/x-moc" = "nvim.desktop";
+    "text/x-chdr" = "nvim.desktop";
+    "text/x-tcl" = "nvim.desktop";
+    "application/javascript" = "nvim.desktop";
+    "application/json" = "nvim.desktop";
+    "application/json+ld" = "nvim.desktop";
+    "application/yaml" = "nvim.desktop";
+    "application/toml" = "nvim.desktop";
+    "application/xml" = "nvim.desktop";
+    "application/x-shellscript" = "nvim.desktop";
+    "application/x-cmake" = "nvim.desktop";
+    "application/x-yaml" = "nvim.desktop";
+    "application/x-tex" = "nvim.desktop";
+    "application/x-ruby" = "nvim.desktop";
+    "application/x-python" = "nvim.desktop";
+    "application/x-perl" = "nvim.desktop";
+    "application/x-lua" = "nvim.desktop";
   };
 }
--- modules/home-manager/programs/cli/neovim.nix
+++ modules/home-manager/programs/cli/neovim.nix
@@ -4,19 +4,82 @@
   pkgs,
   ...
 }: let
-  cfg = config.programs.neovim.pvim;
+  cfg = config.programs.neovim;
+  inherit (lib) mkOption mkIf types getExe;
 in {
-  options.programs.neovim.pvim = {
-    enable = lib.mkEnableOption "pvim";
-
-    package = lib.mkOption {
-      type = lib.types.package;
-      default = pkgs.pvim;
-      description = "The pvim package";
+  options.programs.neovim = {
+    pvim = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = "The pvim package";
+      };
+      package = mkOption {
+        type = types.package;
+        default = pkgs.pvim;
+        description = "The pvim package";
+      };
     };
+    createDesktopEntry = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to create a desktop entry for Neovim";
+    };
+    desktopEntry = let
+      entry = {
+        name = "Neovim";
+        genericName = "Text Editor";
+        comment = "Edit text files";
+        exec = "${getExe pkgs.handlr-regex} launch \"x-scheme-handler/terminal\" -- -e nvim %F";
+        icon = "nvim";
+        mimeType = [
+          "text/english"
+          "text/plain"
+          "text/x-makefile"
+          "text/x-java"
+          "text/x-pascal"
+          "text/x-lua"
+          "text/x-c"
+          "text/x-c++"
+          "text/x-csrc"
+          "text/x-csharp"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-tex"
+          "text/x-moc"
+          "text/x-chdr"
+          "text/x-tcl"
+          "application/javascript"
+          "application/json"
+          "application/json+ld"
+          "application/yaml"
+          "application/toml"
+          "application/xml"
+          "application/x-shellscript"
+          "application/x-cmake"
+          "application/x-yaml"
+          "application/x-tex"
+          "application/x-ruby"
+          "application/x-python"
+          "application/x-perl"
+          "application/x-lua"
+        ];
+        terminal = false;
+        type = "Application";
+      };
+    in
+      mkOption {
+        type = types.attrs;
+        default = entry;
+        description = "The desktop entry for Neovim";
+      };
   };
-  config = lib.mkIf cfg.enable {
-    home.packages = [cfg.package];
-    programs.zoxide.enable = lib.mkDefault true;
+  config = mkIf cfg.enable {
+    xdg.desktopEntries = mkIf cfg.createDesktopEntry {
+      nvim = cfg.desktopEntry;
+    };
+
+    home.packages = mkIf cfg.pvim.enable [cfg.pvim.package];
+    programs.zoxide.enable = mkIf cfg.pvim.enable true;
   };
 }
