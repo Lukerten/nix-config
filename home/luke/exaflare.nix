76008728a79768f62f3ec4b6749a45c911c5f149
Lucas Brendgen <lc.brendgen@gmail.com>
2024-11-06 21:56:24 +0100

    fix(home/exaflare): fixing monitor setup

--- home/luke/exaflare.nix
+++ home/luke/exaflare.nix
@@ -9,17 +9,16 @@
   # |   | |------| |------|
   # |D1 | |  D2  | |  D3  |
   # |   | |------| |------|
-  # -----    
+  # -----
 
   wallpaper = pkgs.wallpapers.aestetic-deer;
   monitors = [
     {
-      name = "DP-2";
-      width = 1080;
-      height = 1920;
+      name = "DP-3";
+      height = 1080;
+      width = 1920;
       enabled = true;
       x = 0;
-      y = -420;
       primary = false;
       workspace = "2";
       orientation = 1;
@@ -30,15 +29,17 @@
       height = 1080;
       enabled = true;
       x = 1080;
+      y = 420;
       primary = true;
       workspace = "1";
     }
     {
-      name = "DP-3";
+      name = "DP-2";
+      enabled = true;
       width = 1920;
       height = 1080;
-      enabled = true;
       x = 3000;
+      y = 420;
       primary = false;
       workspace = "3";
     }
