db8978d7d6e47fc9835ea29b2c24ae9f52019c2c
Lucas Brendgen <lc.brendgen@gmail.com>
2024-11-09 01:10:17 +0100

    fix(hosts/exaflare): change hardware config

--- hosts/exaflare/hardware-configuration.nix
+++ hosts/exaflare/hardware-configuration.nix
@@ -1,34 +1,29 @@
-{
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  ...
-}: {
-  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+{ config, lib, pkgs, modulesPath, ... }:{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot = {
-    initrd = {
-      availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
-      kernelModules = [];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/842d0a30-8069-49e0-bca7-4e66f819f7fa";
+      fsType = "ext4";
     };
-    kernelModules = ["kvm-amd"];
-    extraModulePackages = [];
-  };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0cc9dce2-6348-4e18-9f75-9f92dcbcee76";
-    fsType = "ext4";
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/0E9D-70CE";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/EC06-1127";
-    fsType = "vfat";
-  };
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/a0f8efa3-6b64-4896-803a-3b1e33e5e0d7"; }
+    ];
 
-  swapDevices = [{device = "/dev/disk/by-uuid/aaee0982-a4a8-48b2-8527-eb2ed20868be";}];
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
