06fe62ddfb886393cae27350ed3e9134242bebc3
Lucas Brendgen <lc.brendgen@gmail.com>
2024-11-11 18:55:09 +0100

    fix(home/exaflare): fixing Display Layout

--- home/luke/exaflare.nix
+++ home/luke/exaflare.nix
@@ -5,41 +5,39 @@
     ./features/games
   ];
 
-  # -----
-  # |   | |------| |------|
-  # |D1 | |  D2  | |  D3  |
-  # |   | |------| |------|
-  # -----
+  # |------| |------| |------|
+  # |  D1  | |  D2  | |  D3  |
+  # |------| |------| |------|
 
   wallpaper = pkgs.wallpapers.funky-red-cyan-landscape;
   monitors = [
     {
-      name = "DP-3";
-      height = 1080;
-      width = 1920;
+      name = "DP-2";
       enabled = true;
+      width = 1920;
+      height = 1080;
       x = 0;
+      y = 0;
       primary = false;
       workspace = "2";
-      orientation = 1;
     }
     {
       name = "HDMI-A-1";
       width = 1920;
       height = 1080;
       enabled = true;
-      x = 1080;
-      y = 420;
+      x = 1920;
+      y = 0;
       primary = true;
       workspace = "1";
     }
     {
-      name = "DP-2";
-      enabled = true;
+      name = "DP-3";
       width = 1920;
       height = 1080;
-      x = 3000;
-      y = 420;
+      enabled = true;
+      x = 3840;
+      y = 0;
       primary = false;
       workspace = "3";
     }
