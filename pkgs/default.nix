e04ddb6f18e36d66c14cdf78a3b0ff5a3bd69ed5
Lucas Brendgen <lc.brendgen@gmail.com>
2024-12-22 00:17:09 +0100

    chore(pkgs/fultimator): removed package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,7 +4,6 @@
   trekscii = pkgs.callPackage ./trekscii {};
   lyrics = pkgs.python3Packages.callPackage ./lyrics {};
   cm4all-vpn = pkgs.callPackage ./cm4all-vpn {};
-  fultimator-desktop = pkgs.callPackage ./fultimator-desktop {};
 
   # Personal scripts
   xpo = pkgs.callPackage ./xpo {};
