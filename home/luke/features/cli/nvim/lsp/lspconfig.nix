774ffc298383a11f89fef4bd4ed97b43c1c8c443
Lucas Brendgen <lc.brendgen@gmail.com>
2024-12-22 16:57:02 +0100

    fix(home/nvim): improved lspconfig

--- home/luke/features/cli/nvim/lsp/lspconfig.nix
+++ home/luke/features/cli/nvim/lsp/lspconfig.nix
@@ -167,83 +167,139 @@ with builtins; let
     ''
     # lua
     ''
-      -- Java language server
-      -- workspace setup
       local home = os.getenv("HOME")
-      local jdtls = require('jdtls')
+      local jdtls = require("jdtls")
       local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle", ".project" }
-      local root_dir = jdtls.setup.find_root(root_markers)
+      local root_dir = require("jdtls.setup").find_root(root_markers)
 
-      -- Function to set root_dir to the parent directory of the .git folder
+      -- If root_dir is nil, use current working directory
+      if root_dir == nil then
+        root_dir = vim.fn.getcwd()
+      end
+
+      local workspace_folder = home .. "/.cache/jdtls/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+      -- Setting up root_dir
       local function get_root_dir()
         return lspconfig.util.root_pattern(unpack(root_markers))(vim.fn.expand('%:p:h'))
       end
 
-      java_on_attach = function(client, bufnr)
-        attach_keymaps(client, bufnr)
-        local opts = { noremap=true, silent=true, buffer = bufnr }
-          vim.keymap.set("n", "<leader>jo", "<Cmd>lua require'jdtls'.organize_imports()<CR>", default_opts("Organize imports", bufnr))
-          vim.keymap.set("n", "<leader>jrv", "<Cmd>lua require'jdtls'.extract_variable()<CR>", default_opts("Extract variable", bufnr))
-          vim.keymap.set("x", "<leader>jrv", "<Esc><Cmd>lua require'jdtls'.extract_variable(true)<CR>", default_opts("Extract variable", bufnr))
-          vim.keymap.set("n", "<leader>jrc", "<Cmd>lua require'jdtls'.extract_constant()<CR>", default_opts("Extract constant", bufnr))
-          vim.keymap.set("x", "<leader>jrc", "<Esc><Cmd>lua require'jdtls'.extract_constant(true)<CR>", default_opts("Extract constant", bufnr))
-          vim.keymap.set("x", "<leader>jrm", "<Esc><Cmd>lua require'jdtls'.extract_method(true)<CR>", default_opts("Extract method", bufnr))
-          vim.lsp.codelens.refresh()
-      end
-
-      local workspace_folder = home .. "/.cache/jdtls/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
       local jdtls_config_dir = home .. "/.config/jdtls_config"
       os.execute("mkdir -p " .. jdtls_config_dir)
 
       -- Copy from nix store to config dir
       os.execute("cp -r ${pkgs.jdt-language-server}/config_linux/* " .. jdtls_config_dir)
 
-      lspconfig.jdtls.setup{
-        capabilities = capabilities;
-        on_attach = java_on_attach,
-        cmd = {
-          "${pkgs.jdt-language-server}/bin/jdtls",
-          "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-          "-Dosgi.bundles.defaultStartLevel=4",
-          "-Dosgi.sharedConfiguration.area=${pkgs.jdt-language-server}/share/config",
-          "-Declipse.product=org.eclipse.jdt.ls.core.product",
-          "-Dlog.protocol=true",
-          "-Dlog.level=ALL",
-          "-Xms1g",
-          "--add-modules=ALL-SYSTEM",
-          "--add-opens",
-          "java.base/java.util=ALL-UNNAMED",
-          "--add-opens",
-          "java.base/java.lang=ALL-UNNAMED",
-          "-jar",
-          "-configuration", jdtls_config_dir,
-          "-data", workspace_folder,
+      -- Debuging
+      local bundles = {}
+      local debug_bundles = vim.split(vim.fn.glob("${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/*.jar"), "\n")
+      local test_bundles = vim.split(vim.fn.glob("${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server/*.jar"), "\n")
+      vim.list_extend(bundles, debug_bundles)
+      -- vim.list_extend(bundles, test_bundles) TODO: Need to look into
+
+      java_on_attach = function(client, bufnr)
+        attach_keymaps(client, bufnr)
+        local opts = { noremap=true, silent=true, buffer = bufnr }
+        vim.keymap.set("n", "<leader>Ljo", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+        vim.keymap.set("n", "<leader>Ljrv", "<Cmd>lua require'jdtls'.extract_variable()<CR>", opts)
+        vim.keymap.set("x", "<leader>Ljrv", "<Esc><Cmd>lua require'jdtls'.extract_variable(true)<CR>", opts)
+        vim.keymap.set("n", "<leader>Ljrc", "<Cmd>lua require'jdtls'.extract_constant()<CR>", opts)
+        vim.keymap.set("x", "<leader>Ljrc", "<Esc><Cmd>lua require'jdtls'.extract_constant(true)<CR>", opts)
+        vim.keymap.set("x", "<leader>Ljrm", "<Esc><Cmd>lua require'jdtls'.extract_method(true)<CR>", opts)
+
+        -- Set autocommands conditional on server_capabilities
+        vim.lsp.codelens.refresh()
+      end
+
+      local config = {
+        flags = {
+          allow_incremental_sync = true,
         };
-        root_dir = get_root_dir;
-        init_options = {
-          bundles = {
-            vim.fn.glob("${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-*.jar", 1),
-            vim.fn.glob("${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server/*.jar", 1),
-            vim.fn.glob("${pkgs.lombok}/share/java/lombok/lombok.jar", 1),
+        capabilities = capabilities,
+        on_attach = java_on_attach,
+      };
+
+      config.settings = {
+        java = {
+          referencesCodeLens = {enabled = true};
+          signatureHelp = { enabled = true };
+          implementationsCodeLens = { enabled = true };
+          contentProvider = { preferred = 'fernflower' };
+          completion = {
+            favoriteStaticMembers = {
+              "org.hamcrest.MatcherAssert.assertThat",
+              "org.hamcrest.Matchers.*",
+              "org.hamcrest.CoreMatchers.*",
+              "org.junit.jupiter.api.Assertions.*",
+              "java.util.Objects.requireNonNull",
+              "java.util.Objects.requireNonNullElse",
+              "org.mockito.Mockito.*"
+            }
+          };
+          sources = {
+            organizeImports = {
+              starThreshold = 9999;
+              staticStarThreshold = 9999;
+            };
+          };
+          configuration = {
+            runtimes = {
+              {
+                name = "JavaSE-17",
+                path = "${pkgs.jdk17}/lib/openjdk/",
+              },
+              {
+                name = "JavaSE-21",
+                path = "${pkgs.jdk21}/lib/openjdk/",
+              },
+            }
           };
         };
-        settings = {
-          java = {
-            referencesCodeLens = {enabled = true};
-            signatureHelp = { enabled = true };
-            implementationsCodeLens = { enabled = true };
-            contentProvider = { preferred = 'fernflower' };
-          },
-        };
-        handlers = {
-          ["language/status"] = function(_, result)
-            if result.value == "error" then
-              vim.lsp.diagnostic.set_loclist({open_loclist = false})
-            end
-          end
-        };
-        filetypes = { "java" };
-      }
+      };
+      config.cmd = {
+        "${pkgs.jdt-language-server}/bin/jdtls",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Dosgi.sharedConfiguration.area=${pkgs.jdt-language-server}/share/config",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+        "-jar",
+        "-configuration", jdtls_config_dir,
+        "-data", workspace_folder,
+      };
+      config.root_dir = get_root_dir()
+      config.on_init = function(client, _)
+        client.notify('workspace/didChangeConfiguration', { settings = config.settings })
+      end
+
+      local extendedClientCapabilities = require'jdtls'.extendedClientCapabilities
+      extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+      config.init_options = {
+        bundles = bundles,
+        extendedClientCapabilities = extendedClientCapabilities;
+      };
+      config.filetypes = {"java"};
+      local nvim_jdtls_group = vim.api.nvim_create_augroup("nvim-jdtls", { clear = true })
+
+      vim.api.nvim_create_autocmd(
+        "FileType",
+        {
+          pattern = { "java" },
+          callback = function()
+            jdtls.start_or_attach(config)
+            jdtls.setup_dap({ hotcodereplace = "auto" })
+            -- jdtls.dap.setup_dap_main_class_configs() TODO: return a nil
+          end,
+          group = nvim_jdtls_group,
+        }
+      )
     ''
     # lua
     ''
@@ -347,6 +403,12 @@ with builtins; let
     # lua
     ''
       -- Python language server
+      lspconfig.pyright.setup{
+        capabilities = capabilities;
+        on_attach = default_on_attach;
+        cmd = {"${pkgs.pyright}/bin/pyright-langserver", "--stdio"}
+      }
+
       lspconfig.pylsp.setup{
         cmd = { '${pkgs.python312Packages.python-lsp-server}/bin/pylsp' },
         filetypes = { 'python' },
@@ -533,6 +595,15 @@ with builtins; let
             https://github.com/redhat-developer/yaml-language-server
           ]],
         },
+      }
+    ''
+    # lua
+    ''
+      -- XML language server
+      lspconfig.lemminx.setup{
+        capabilities = capabilities;
+        on_attach = default_on_attach,
+        cmd = { "${pkgs.lemminx}/bin/lemminx" },
       }
     ''
   ];
--- home/luke/features/cli/nvim/lsp/null-ls.nix
+++ home/luke/features/cli/nvim/lsp/null-ls.nix
@@ -53,9 +53,11 @@ with builtins; let
     ''
     # lua
     ''
+      table.insert(ls_sources, null_ls.builtins.code_actions.gitsigns)
+    ''
+    ''
       -- Python formatting: black
       table.insert(ls_sources, null_ls.builtins.formatting.black.with({
-        filetypes = { "python" },
         command = "${pkgs.black}/bin/black",
       }))
     ''
