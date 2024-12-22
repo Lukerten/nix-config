91055f511f88c31dc351cc45259e267ca783d112
Lucas Brendgen <lc.brendgen@gmail.com>
2024-12-22 16:56:22 +0100

    fix(home/nvim): improved gitsigns

--- home/luke/features/cli/nvim/visuals/gitsigns.nix
+++ home/luke/features/cli/nvim/visuals/gitsigns.nix
@@ -8,21 +8,14 @@
         ''
           require('gitsigns').setup {
             watch_gitdir = {
-              interval = 1000,
-              follow_files = true
-            },
-            signs = {
-              add = { text = '+' },
-              change = { text = '~' },
-              delete = { text = '_' },
-              topdelete = { text = '‾' },
-              changedelete = { text = '~' },
+            interval = 1000,
+            follow_files = true
             },
             attach_to_untracked = true,
             current_line_blame = false,
             current_line_blame_opts = {
               virt_text = true,
-              virt_text_pos = 'eol',
+              virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
               delay = 1000,
               ignore_whitespace = false,
             },
@@ -32,7 +25,6 @@
             status_formatter = nil,
             max_file_length = 3000,
             preview_config = {
-              -- Options passed to nvim_open_win
               border = 'single',
               style = 'minimal',
               relative = 'cursor',
@@ -41,12 +33,17 @@
             },
             on_attach = function(bufnr)
               local gs = package.loaded.gitsigns
-
               local function map(mode, l, r, opts)
                 opts = opts or {}
                 opts.buffer = bufnr
                 vim.keymap.set(mode, l, r, opts)
               end
+
+              -- actions
+              map('n', '<leader>gb', function() gs.blame_line{full=true} end)
+
+              -- Text objects
+              map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
             end
           }
         '';
