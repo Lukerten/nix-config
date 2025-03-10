d75225116bf4240183cb15743d36f010dfe1ecf0
Lucas Brendgen <lc.brendgen@gmail.com>
2025-03-11 00:51:37 +0100

    feat(home/cli): improving tmux config

--- home/luke/features/cli/tmux.nix
+++ home/luke/features/cli/tmux.nix
@@ -1,129 +1,165 @@
-{pkgs, ...}: {
-  programs.tmux = {
-    enable = true;
-    shortcut = "a";
-    baseIndex = 1;
-    escapeTime = 0;
-    historyLimit = 10000;
-    keyMode = "vi";
-    terminal = "screen-256color";
-    plugins = with pkgs; [
-      tmuxPlugins.cpu
-      tmuxPlugins.battery
-      tmuxPlugins.net-speed
-      tmuxPlugins.better-mouse-mode
-    ];
-    extraConfig = ''
-      # Allow mouse interaction
-      set-option -g mouse on
+{pkgs, ...}: let
+  t-smart-manager =
+    pkgs.tmuxPlugins.mkTmuxPlugin
+    {
+      pluginName = "t-smart-tmux-session-manager";
+      version = "unstable-2023-01-06";
+      rtpFilePath = "t-smart-tmux-session-manager.tmux";
+      src = pkgs.fetchFromGitHub {
+        owner = "joshmedeski";
+        repo = "t-smart-tmux-session-manager";
+        rev = "a1e91b427047d0224d2c9c8148fb84b47f651866";
+        sha256 = "sha256-HN0hJeB31MvkD12dbnF2SjefkAVgtUmhah598zAlhQs=";
+      };
+    };
+  tmux-nvim =
+    pkgs.tmuxPlugins.mkTmuxPlugin
+    {
+      pluginName = "tmux.nvim";
+      version = "unstable-2023-01-06";
+      src = pkgs.fetchFromGitHub {
+        owner = "aserowy";
+        repo = "tmux.nvim/";
+        rev = "57220071739c723c3a318e9d529d3e5045f503b8";
+        sha256 = "sha256-zpg7XJky7PRa5sC7sPRsU2ZOjj0wcepITLAelPjEkSI=";
+      };
+    };
+  tmux-browser =
+    pkgs.tmuxPlugins.mkTmuxPlugin
+    {
+      pluginName = "tmux-browser";
+      version = "unstable-2023-01-06";
+      src = pkgs.fetchFromGitHub {
+        owner = "ofirgall";
+        repo = "tmux-browser";
+        rev = "c3e115f9ebc5ec6646d563abccc6cf89a0feadb8";
+        sha256 = "sha256-ngYZDzXjm4Ne0yO6pI+C2uGO/zFDptdcpkL847P+HCI=";
+      };
+    };
 
-      bind C-e send-keys 'vi .' Enter
-
-      # enable true colors
-      set-option -sa terminal-overrides ',*:RGB'
-      set-option -ga terminal-overrides ',*:Tc'
-
-      # more space to bottom row
-      setw -g pane-border-status top
-      setw -g pane-border-format ""
-      # TODO set color with color generator
-      #set -g pane-active-border-style bg=default,fg=brightblack
-
-      # disable repetition
-      set-option -g repeat-time 0
-
-      # renumbers windows once one is killed
-      set-option -g renumber-windows on
-
-      # Activity Monitoring
-      setw -g monitor-activity off
-      set -g visual-activity off
-
-      set-option -g set-titles on
-      set-option -g set-titles-string '#T - #W'
-
-      # all input in all panes synchronizes
-      bind p set-window-option synchronize-panes
-
-      # only useful if not using NixOS
-      #bind r source-file ~/.tmux.conf
-
-      # pane movement shortcuts
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
-
-      # Resize pane shortcuts
-      bind -r C-k select-window -t :-
-      bind -r C-j select-window -t :+
-
-      bind C-u killp
-
-      # Resize pane shortcuts
-      bind -r H resize-pane -L 10
-      bind -r J resize-pane -D 10
-      bind -r K resize-pane -U 10
-      bind -r L resize-pane -R 10
-
-      # split window and fix path for tmux 1.9
-      bind n split-window -h -c "#{pane_current_path}"
-      bind y split-window -v -c "#{pane_current_path}"
-
-
-      # Styling
-      set -g visual-activity off
-      set -g visual-bell off
-      set -g visual-silence off
-      setw -g monitor-activity off
-      set -g bell-action none
-      setw -g clock-mode-color color1
-      setw -g mode-style 'fg=color1 bg=color18 bold'
-      set -g pane-border-style 'fg=color1'
-      set -g pane-active-border-style 'fg=color3'
-
-      # statusbar
-      setw -g clock-mode-color yellow
-      setw -g mode-style 'fg=black bg=red bold'
-
-      # panes
-      set -g pane-border-style 'fg=red'
-      set -g pane-active-border-style 'fg=yellow'
-
-      # statusbar
-      set -g status-position top
-      set -g status-justify left
-      set -g status-style 'fg=red'
-
-      set -g status-left '#{?client_prefix,#[fg=green],#[fg=red]} '
-      set -g status-left-length 10
-
-
-      set -g status-right-style 'fg=black bg=yellow'
-      set -g status-right '#[reverse]#[noreverse]%Y-%m-%d %H:%M#[reverse]#[noreverse]'
-
-      setw -g window-status-current-style 'fg=black bg=red'
-      setw -g window-status-current-format '#[reverse]#[noreverse]#I #W #F#[reverse]#[noreverse]'
-
-      setw -g window-status-style 'fg=red bg=black'
-      setw -g window-status-separator '|'
-      setw -g window-status-format ' #I #[fg=white]#W #[fg=yellow]#F '
-
-      setw -g window-status-bell-style 'fg=yellow bg=red bold'
-
-      # messages
-      set -g message-style 'fg=yellow bg=black bold'
+  tmux-super-fingers =
+    pkgs.tmuxPlugins.mkTmuxPlugin
+    {
+      pluginName = "tmux-super-fingers";
+      version = "unstable-2023-01-06";
+      src = pkgs.fetchFromGitHub {
+        owner = "artemave";
+        repo = "tmux_super_fingers";
+        rev = "2c12044984124e74e21a5a87d00f844083e4bdf7";
+        sha256 = "sha256-cPZCV8xk9QpU49/7H8iGhQYK6JwWjviL29eWabuqruc=";
+      };
+    };
+in {
+  # TODO: what if this is defined in another file? Merge it!
+  programs.fish = {
+    shellInit = ''
+      fish_add_path ${t-smart-manager}/share/tmux-plugins/t-smart-tmux-session-manager/bin/
     '';
   };
-  xdg.desktopEntries = {
-    tmux = {
-      name = "Tmux";
-      genericName = "Terminal Multiplexer";
-      comment = "Terminal Multiplexer";
-      exec = "tmux";
-      icon = "tmux";
-      terminal = true;
-      type = "Application";
-    };
+
+  home.packages = with pkgs; [
+    lsof
+  ];
+
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    plugins = with pkgs; [
+      tmux-nvim
+      tmuxPlugins.tmux-thumbs
+      # TODO: why do I have to manually set this
+      {
+        plugin = t-smart-manager;
+        extraConfig = ''
+          set -g @t-fzf-prompt '  '
+          set -g @t-bind "T"
+        '';
+      }
+      {
+        plugin = tmux-super-fingers;
+        extraConfig = "set -g @super-fingers-key f";
+      }
+      {
+        plugin = tmux-browser;
+        extraConfig = ''
+          set -g @browser_close_on_deattach '1'
+        '';
+      }
+
+      tmuxPlugins.sensible
+      # must be before continuum edits right status bar
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'frappe'
+          set -g @catppuccin_window_tabs_enabled on
+          set -g @catppuccin_date_time "%H:%M"
+        '';
+      }
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-vim 'session'
+          set -g @resurrect-strategy-nvim 'session'
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-boot 'on'
+          set -g @continuum-save-interval '10'
+        '';
+      }
+      tmuxPlugins.better-mouse-mode
+      tmuxPlugins.yank
+    ];
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB"
+
+      set-option -g prefix C-a
+      unbind-key C-b
+      bind-key C-a send-prefix
+
+      set -g mouse on
+
+      # Change splits to match nvim and easier to remember
+      # Open new split at cwd of current split
+      unbind %
+      unbind '"'
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+
+      # Use vim keybindings in copy mode
+      set-window-option -g mode-keys vi
+
+      # v in copy mode starts making selection
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      # Escape turns on copy mode
+      bind Escape copy-mode
+
+      # Easier reload of config
+      bind r source-file ~/.config/tmux/tmux.conf
+
+      set-option -g status-position top
+
+      # make Prefix p paste the buffer.
+      unbind p
+      bind p paste-buffer
+
+      # Bind Keys
+      bind-key -T prefix C-g split-window \
+        "$SHELL --login -i -c 'navi --print | head -c -1 | tmux load-buffer -b tmp - ; tmux paste-buffer -p -t {last} -b tmp -d'"
+      bind-key -T prefix C-l switch -t notes
+      bind-key -T prefix C-d switch -t dotfiles
+      bind-key e send-keys "tmux capture-pane -p -S - | nvim -c 'set buftype=nofile' +" Enter
+    '';
   };
 }
