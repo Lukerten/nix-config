a655b9cf7e2403ab544073f95657cf69fbc8288a
Lucas Brendgen <lc.brendgen@gmail.com>
2025-03-29 19:58:36 +0100

    wip(hosts/system/inception): started adding jellyfin

---
+++ hosts/systems/inception/jellyfin/default.nix
@@ -0,0 +1,26 @@
+{pkgs, ...}: {
+  imports = [./package.nix];
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.traefik.dynamicConfigOptions = {
+    http.routers = {
+      jellyfin = {
+        rule = " Host(`jf.local`)";
+        entryPoints = ["web"];
+        service = "jellyfin";
+        tls = false;
+      };
+    };
+    http.services = {
+      jellyfin = {
+        loadBalancer.servers = [
+          {url = "http://127.0.0.1:8096";}
+        ];
+      };
+    };
+  };
+}
