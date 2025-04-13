d92db99a0cb05593a860c6c10e3459daf4e684e5
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-04-13 20:58:17 +0200

    feat(home/nvim): prepping avante-nvim plugin finally ... neovim vibecoding

--- home/luke/features/nvim/copilot/default.nix
+++ home/luke/features/nvim/copilot/default.nix
@@ -1,15 +1,12 @@
 {pkgs, ...}: {
+  imports = [
+    ./avante.nix
+  ];
+
   programs.nixvim = {
     plugins = {
       copilot-lua = {
         enable = true;
-      };
-
-      copilot-chat = {
-        enable = true;
-        settings = {
-          model = "claude-3.7-sonnet";
-        };
       };
     };
     extraPackages = with pkgs; [nodejs-slim];
