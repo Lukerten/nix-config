f0b08d99ba69446195f5d8dff63184e6534c5d3e
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-04-13 22:23:28 +0200

    feat(home/nvim): cleaning up avante config

--- home/luke/features/nvim/copilot/avante.nix
+++ home/luke/features/nvim/copilot/avante.nix
@@ -1,68 +1,55 @@
 {
-  programs.nixvim = {
-    plugins.avante = {
-      # TODO: set this up!
-      enable = true;
+  programs.nixvim.plugins.avante = {
+    enable = true;
 
-      lazyLoad.settings.event = ["BufEnter"];
-      settings = {
-        mappings = {
-          files = {
-            add_current = "<leader>a.";
-          };
-        };
-        copilot = {
-          model = "claude-3.7-sonnet";
-          endpoint = "https://api.githubcopilot.com";
-          allow_insecure = false;
-          timeout = 10 * 60 * 1000;
-          temperature = 0;
-          max_completion_tokens = 1000000;
-          reasoning_effort = "high";
-        };
-        diff = {
-          autojump = true;
-          debug = false;
-          list_opener = "copen";
-        };
-        highlights = {
-          diff = {
-            current = "DiffText";
-            incoming = "DiffAdd";
-          };
-        };
-        hints = {
-          enabled = true;
-        };
-        mappings = {
-          diff = {
-            both = "cb";
-            next = "]x";
-            none = "c0";
-            ours = "co";
-            prev = "[x";
-            theirs = "tc";
-          };
-        };
-        provider = "copilot";
-        windows = {
-          sidebar_header = {
-            align = "center";
-            rounded = true;
-          };
-          width = 30;
-          wrap = true;
+    lazyLoad.settings.event = ["BufEnter"];
+    settings = {
+      mappings = {
+        files = {
+          add_current = "<leader>a.";
         };
       };
-    };
-
-    plugins.render-markdown = {
-      enable = true;
-      settings = {
-        file_types = [
-          "markdown"
-          "Avante"
-        ];
+      copilot = {
+        model = "claude-3.7-sonnet";
+        endpoint = "https://api.githubcopilot.com";
+        allow_insecure = false;
+        timeout = 10 * 60 * 1000;
+        temperature = 0;
+        max_completion_tokens = 1000000;
+        reasoning_effort = "high";
+      };
+      diff = {
+        autojump = true;
+        debug = false;
+        list_opener = "copen";
+      };
+      highlights = {
+        diff = {
+          current = "DiffText";
+          incoming = "DiffAdd";
+        };
+      };
+      hints = {
+        enabled = true;
+      };
+      mappings = {
+        diff = {
+          both = "cb";
+          next = "]x";
+          none = "c0";
+          ours = "co";
+          prev = "[x";
+          theirs = "tc";
+        };
+      };
+      provider = "copilot";
+      windows = {
+        sidebar_header = {
+          align = "center";
+          rounded = true;
+        };
+        width = 30;
+        wrap = true;
       };
     };
   };
