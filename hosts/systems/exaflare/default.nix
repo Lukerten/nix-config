c00b9fcf901cc50ad7177d383691cdaa28379d8a
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-04-16 22:43:27 +0200

    wip(hosts/systems): adding small configs to exaflare host

--- hosts/systems/exaflare/default.nix
+++ hosts/systems/exaflare/default.nix
@@ -2,6 +2,8 @@
   imports = [
     inputs.hardware.nixosModules.common-cpu-amd
     inputs.hardware.nixosModules.common-gpu-amd
+    inputs.hardware.nixosModules.common-hidpi
+    inputs.hardware.nixosModules.common-pc-ssd
     ./hardware-configuration.nix
 
     # common configuration
@@ -32,29 +34,27 @@
       enable = true;
       enable32Bit = true;
     };
-    bluetooth.enable = true;
     uinput.enable = true;
   };
 
   programs = {
     adb.enable = true;
     dconf.enable = true;
-    light.enable = true;
   };
 
   programs.gamemode = {
     enable = true;
-    settings = {
-      general = {
-        softrealtime = "on";
-        inhibit_screensaver = 1;
-      };
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
-    };
+    # settings = {
+    #   general = {
+    #     softrealtime = "on";
+    #     inhibit_screensaver = 1;
+    #   };
+    #   gpu = {
+    #     apply_gpu_optimisations = "accept-responsibility";
+    #     gpu_device = 0;
+    #     amd_performance_level = "high";
+    #   };
+    # };
   };
 
   system.stateVersion = "24.05";
