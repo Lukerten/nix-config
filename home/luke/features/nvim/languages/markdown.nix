5f58b92eaf478816bc6bb2553dbaf928db907bf5
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-04-16 09:18:51 +0200

    feat(home/nvim): fixing issues with markdown diagram rendering

--- home/luke/features/nvim/languages/markdown.nix
+++ home/luke/features/nvim/languages/markdown.nix
@@ -1,72 +1,98 @@
-{
-  programs.nixvim.plugins = {
-    lsp.servers.marksman.enable = true;
-    none-ls.sources.formatting.prettier.enable = true;
-    conform-nvim.settings.formatters_by_ft.markdown = {
-      __unkeyed-1 = "prettierd";
-      __unkeyed-2 = "prettier";
-      stop_after_first = true;
-    };
+{pkgs, ...}: {
+  programs.nixvim = {
+    plugins = {
+      lsp.servers.marksman.enable = true;
+      none-ls.sources.formatting.prettier.enable = true;
+      conform-nvim.settings.formatters_by_ft.markdown = {
+        __unkeyed-1 = "prettierd";
+        __unkeyed-2 = "prettier";
+        stop_after_first = true;
+      };
 
-    # Snippet Support
-    luasnip.fromSnipmate = [
-      {
-        paths = ../snippets/store/snippets/markdown.snippets;
-        include = ["markdown"];
-      }
-    ];
+      # Snippet Support
+      luasnip.fromSnipmate = [
+        {
+          paths = ../snippets/store/snippets/markdown.snippets;
+          include = ["markdown"];
+        }
+      ];
 
-    # Extra Plug: render Markdown
-    render-markdown = {
-      enable = true;
-      settings = {
-        render_modes = true;
-        signs.enabled = false;
-        bullet = {
-          icons = [
-            "◆ "
-            "• "
-            "• "
+      # Extra Plug: render Markdown
+      render-markdown = {
+        enable = true;
+        settings = {
+          render_modes = true;
+          signs.enabled = false;
+          bullet = {
+            icons = [
+              "◆ "
+              "• "
+              "• "
+            ];
+            right_pad = 1;
+          };
+          heading = {
+            sign = false;
+            width = "full";
+            position = "inline";
+            border = true;
+            icons = [
+              "1 "
+              "2 "
+              "3 "
+              "4 "
+              "5 "
+              "6 "
+            ];
+          };
+          code = {
+            sign = false;
+            width = "block";
+            position = "right";
+            language_pad = 2;
+            left_pad = 2;
+            right_pad = 2;
+            border = "thick";
+            above = " ";
+            below = " ";
+          };
+          file_types = [
+            "markdown"
+            "neorg"
+            "Avante"
           ];
-          right_pad = 1;
         };
-        heading = {
-          sign = false;
-          width = "full";
-          position = "inline";
-          border = true;
-          icons = [
-            "1 "
-            "2 "
-            "3 "
-            "4 "
-            "5 "
-            "6 "
+      };
+
+      # Extra Plug: Image
+      image.enable = true;
+
+      # Extra Plug: Diagram
+      diagram = {
+        enable = true;
+        settings = {
+          integrations = [
+            {
+              __raw = "require('diagram.integrations.markdown')";
+            }
+            {
+              __raw = "require('diagram.integrations.neorg')";
+            }
           ];
+          renderer_options = {
+            d2.theme_id = 1;
+            mermaid.theme = "forest";
+            plantuml.charset = "utf-8";
+            gnuplot = {
+              size = "800,600";
+              theme = "dark";
+            };
+          };
         };
-        code = {
-          sign = false;
-          width = "block";
-          position = "right";
-          language_pad = 2;
-          left_pad = 2;
-          right_pad = 2;
-          border = "thick";
-          above = " ";
-          below = " ";
-        };
-        file_types = [
-          "markdown"
-          "neorg"
-          "Avante"
-        ];
       };
     };
-
-    # Extra Plug: Image
-    image.enable = true;
-
-    # Extra Plug: Diagram
-    diagram.enable = true;
+    extraPackages = with pkgs; [
+      plantuml-c4
+    ];
   };
 }
