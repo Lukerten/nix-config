eb242f024ad1992ef09009501ad2a5ed73f25782
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-05-03 17:04:58 +0200

    feat(pkgs/fultimator-desktop): bump version to 1.0.1

--- pkgs/fultimator-desktop/default.nix
+++ pkgs/fultimator-desktop/default.nix
@@ -4,10 +4,10 @@
   fetchurl,
 }: let
   pname = "fultimator";
-  version = "1.0.0";
+  version = "1.0.1";
   src = fetchurl {
     url = "https://github.com/fultimator/fultimator-desktop/releases/download/v${version}/Fultimator-Linux-x86_64-${version}.AppImage";
-    sha256 = "32anuCguW39R0bL/lvFT8440tsnOfN1Rn0cB6oKcHcs=";
+    sha256 = "1zx17mmykzh9959hdd6p426vxcb6hqka00wnnpc8sy2ll4ixf3x5";
   };
   appimageContents = appimageTools.extractType1 {inherit pname version src;};
 in
@@ -15,10 +15,7 @@ in
     inherit pname version src;
 
     extraInstallCommands = ''
-      ls
-      ls $out/bin
-
-      install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
+        install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
       substituteInPlace $out/share/applications/${pname}.desktop \
       --replace-fail 'Exec=AppRun' 'Exec=${pname}'
       install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/0x0/apps/${pname}.png -t $out/share/icons/hicolor/512x512/apps
@@ -26,7 +23,7 @@ in
 
     meta = {
       description = "Fabula Ultima Character Sheet Manager";
-      homepage = "https://github.com/ZUGFeRD/quba-viewer";
+      homepage = "https://fultimator.com/";
       downloadPage = "https://github.com/fultimator/fultimator-desktop";
       license = lib.licenses.gpl3;
       sourceProvenance = with lib.sourceTypes; [binaryNativeCode];
